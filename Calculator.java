db83176f192ac401c096fcdd2c023827290aee11
IrinaFF <StratosferA5551>
2016-09-05 23:08:21 +0500

    #

--- Calculator.java
+++ Calculator.java
@@ -1,7 +1,36 @@
-package PACKAGE_NAME;
-
 /**
- * Created by Irina on 03.09.2016.
+ * Класс реализует калькулятор
+ * ДЗ 2. Создать класс Calculator.
+     Добавить два класс клиента ArgRunner, InteractRunner.
+     ArgRunner - данные получает из параметров запуска программы.
+     InteractRunner - данные вводит пользователь.
+     Добавить возможность выборка операции. Валидацию. Повторное использование результат вычисления.
  */
 public class Calculator {
+    /*
+    Результат вычисления
+    */
+    private int result;
+    /**
+     * Суммируем аргументы
+     * @param params Аргументы суммирования.
+     */
+    public void add(int ... params) {
+        for (Integer param : params) {
+            this.result += param;
+        }
+    }
+    /**
+     * Получить результат.
+     * @return результат вычисления.
+     */
+    public int getResult() {
+        return this.result;
+    }
+    /**
+    * Очистить результат вычисления.
+    */
+    public void cleanResult() {
+        this.result = 0;
+    }
 }
