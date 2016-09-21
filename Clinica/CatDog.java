0f1b4461194729abaaf707d704f9c6d4b735fa47
IrinaFF <irina.fatkullina@gmail.com>
2016-09-21 21:23:35 +0500

    к последнему

--- Clinica/CatDog.java
+++ Clinica/CatDog.java
@@ -6,14 +6,23 @@ package Clinica;
  * @since 17.09.2016
  **/
 
-public class CatDog  {
-
+public class CatDog implements Pet {
+    private Pet cat; //переменная для ссылки на объект реализующий интерфейс
+    private Pet dog; //переменная для ссылки на объект реализующий интерфейс
     /**
      * Базовая реализация питомца
      */
-    private Pet pet;
-
-    public CatDog(final Pet pet) {
-        this.pet = pet;
+    public CatDog( Pet cat, Pet dog) {
+        this.cat = cat;
+        this.dog = dog;
+    };
+    @Override
+    public void makeSound(){
+        this.cat.makeSound();
+        this.dog.makeSound();
+    };
+    @Override
+    public  String getName(){
+        return String.format("%s say: %s", this.cat.getName(), this.dog.getName());
     };
 }
