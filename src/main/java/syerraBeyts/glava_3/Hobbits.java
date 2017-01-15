4cb0ce4494ae93e041a1f7ba54b9fc87b41ba87b
IrinaFF <StratosferA5551>
2017-01-15 23:15:01 +0500

    книга глава 4

--- src/main/java/syerraBeyts/glava_3/Hobbits.java
+++ src/main/java/syerraBeyts/glava_3/Hobbits.java
@@ -8,4 +8,23 @@ package syerraBeyts.glava_3;
  **/
 
 public class Hobbits {
+    String name;
+    public static void main(String[] args) {
+        Hobbits[] h = new Hobbits[3];
+        int z = 0;
+        while ( z < h.length) {
+
+            h[z] = new Hobbits();
+            h[z].name = "Бильбо";
+
+            if (z == 1) {
+                h[z].name = "Фродо";
+            }
+            if (z == 2) {
+                h[z].name = "Сэм";
+            }
+            System.out.println(h[z].name + " - " + "имя хорошего хоббита");
+            z++;
+        }
+   }
 }
