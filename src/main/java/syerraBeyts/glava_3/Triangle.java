1e2d48a656a7454bea9b52545e35cac90fe445dd
IrinaFF <StratosferA5551>
2017-01-15 23:14:15 +0500

    книга глава 4

---
+++ src/main/java/syerraBeyts/glava_3/Triangle.java
@@ -0,0 +1,11 @@
+package syerraBeyts.glava_3;
+
+/**
+ * TODO: comment
+ *
+ * @autor irinaff
+ * @since 14.01.2017
+ **/
+
+public class Triangle {
+}
---
+++ src/main/java/syerraBeyts/glava_4/Pazzle4.java
@@ -0,0 +1,11 @@
+package syerraBeyts.glava_4;
+
+/**
+ * TODO: comment
+ *
+ * @autor irinaff
+ * @since 15.01.2017
+ **/
+
+public class Pazzle4 {
+}
