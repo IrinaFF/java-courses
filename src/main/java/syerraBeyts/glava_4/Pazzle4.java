8dc9a663a84b9390e48f8f37d48b59f1474a7b0f
IrinaFF <StratosferA5551>
2017-01-15 23:15:07 +0500

    книга глава 4

--- src/main/java/syerraBeyts/glava_4/Pazzle4.java
+++ src/main/java/syerraBeyts/glava_4/Pazzle4.java
@@ -8,4 +8,34 @@ package syerraBeyts.glava_4;
  **/
 
 public class Pazzle4 {
+    public static void main(String[] args){
+        //
+        int y = 1;
+        int x = 0;
+        int result = 0;
+        while (x < 6) {
+            //
+            //
+            y = y * 10;
+            //
+        }
+        x = 6;
+        while (x > 0) {
+            //
+            result = result + 1;//
+            //
+        }
+        System.out.println("Результат " + result);
+    }
+}
+
+class Pazzle4b {
+    int ivar;
+    public int doStaff(int factor) {
+        if (ivar > 100) {
+            return factor;
+        } else {
+            return factor;
+        }
+    }
 }
