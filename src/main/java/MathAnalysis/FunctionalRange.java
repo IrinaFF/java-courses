68cd76dd8ba982661aba4cf6f75ecf4361017b95
IrinaFF <StratosferA5551>
2017-02-07 09:25:47 +0500

    примеры Бейтс

---
+++ src/main/java/MathAnalysis/FunctionalRange.java
@@ -0,0 +1,16 @@
+package MathAnalysis;
+
+/**
+ * функциональный ряд
+ * @autor irinaff
+ * @since 05.02.2017
+ **/
+
+public class FunctionalRange extends Range {
+    int bilet = 4;
+
+    public void theoremNFR() {
+        System.out.println("4.Теорема о непрерывности суммы функционального ряда");
+    }
+
+}
---
+++ src/main/java/MathAnalysis/Range.java
@@ -0,0 +1,10 @@
+package MathAnalysis;
+
+/**
+ * ряд
+ * @autor irinaff
+ * @since 05.02.2017
+ **/
+
+public class Range {
+}
