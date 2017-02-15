26fdb433bd6335921f4b7f61a16f398f66d58b6a
IrinaFF <StratosferA5551>
2017-02-16 00:54:34 +0500

    oracle docs, thread: examples

---
+++ src/main/java/oracleDocs/thread/GuardedBlocks/Producer.java
@@ -0,0 +1,34 @@
+package oracleDocs.thread.GuardedBlocks;
+
+/**
+ * Created by Irina on 16.02.2017.
+ */
+import java.util.Random;
+
+public class Producer implements Runnable {
+    private Drop drop;
+
+    public Producer(Drop drop) {
+        this.drop = drop;
+    }
+
+    public void run() {
+        String importantInfo[] = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+        Random random = new Random();
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            drop.put(importantInfo[i]);
+            try {
+                Thread.sleep(random.nextInt(5000));
+            } catch (InterruptedException e) {}
+        }
+        drop.put("DONE");
+    }
+}
---
+++ src/main/java/oracleDocs/thread/SleepMessages.java
@@ -0,0 +1,25 @@
+package oracleDocs.thread;
+
+/**
+ * Created by Irina on 13.02.2017.
+ */
+public class SleepMessages {
+    public static void main(String args[])
+            throws InterruptedException {
+        String importantInfo[] = {
+                "Mares eat oats",
+                "Does eat oats",
+                "Little lambs eat ivy",
+                "A kid will eat ivy too"
+        };
+
+        for (int i = 0;
+             i < importantInfo.length;
+             i++) {
+            //Pause for 4 seconds
+            Thread.sleep(4000);
+            //Print a message
+            System.out.println(importantInfo[i]);
+        }
+    }
+}
