926007f5acd18ec3c3bef6991c7584433a3d59d3
IrinaFF <StratosferA5551>
2017-02-20 23:43:33 +0500

    petrarcentev_lesson 11

---
+++ src/main/java/ru/lesson/lessons/lesson_11/CatDog.java
@@ -0,0 +1,47 @@
+package ru.lesson.lessons.lesson_11;
+
+/**
+ * Реализация котопса
+ * @autor irinaff
+ * @since 17.09.2016
+ **/
+
+public class CatDog implements Pet {
+    private Pet cat; //переменная для ссылки на объект реализующий интерфейс
+    private Pet dog; //переменная для ссылки на объект реализующий интерфейс
+
+    public CatDog(Pet cat, Pet dog) {
+        this.cat = cat;
+        this.dog = dog;
+    };
+
+    /**
+     * {@inheritDoc}
+     * @return
+     */
+    //@Override
+    public void makeSound(){
+        this.cat.makeSound();
+        this.dog.makeSound();
+    };
+    /**
+     * {@inheritDoc}
+     * @return
+     */
+    //@Override
+    public  String getName(){
+        return String.format("%s + %s", this.cat.getName(), this.dog.getName());
+    };
+
+    /**
+     * Сменить имя питомца
+     * */
+    public void changeName(String nameCat, String nameDog) {
+        this.cat.changeName(nameCat);
+        this.dog.changeName(nameDog);
+    };
+
+    public void changeName(String nameCat) {
+        this.cat.changeName(nameCat);
+    };
+}
---
+++ src/main/java/ru/lesson/lessons/lesson_11/Pet.java
@@ -0,0 +1,24 @@
+package ru.lesson.lessons.lesson_11;
+
+/** Описывает поведение питомца
+ * @autor irinaff
+ * @since 17.09.2016
+ **/
+
+public interface Pet{
+
+    /**
+     * Издавать звук
+     */
+    public void makeSound();// {
+    /**
+     * Получить имя питомца
+     * */
+    String getName();
+
+    /**
+     * Сменить имя питомца
+     * */
+    public void changeName(String name);
+}
+
