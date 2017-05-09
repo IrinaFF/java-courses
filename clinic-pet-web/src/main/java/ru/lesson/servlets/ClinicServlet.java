dd47b58430db982426371f1422ea142c013eb96a
IrinaFF <irina.fatkullina@gmail.com>
2017-05-09 15:30:05 +0500

    урок 14. Клиент-сервер. Поиск клиента, отображение

--- clinic-pet-web/src/main/java/ru/lesson/servlets/ClinicServlet.java
+++ clinic-pet-web/src/main/java/ru/lesson/servlets/ClinicServlet.java
@@ -1,7 +1,7 @@
 package ru.lesson.servlets;
 
 import com.sun.net.httpserver.HttpServer;
-import ru.lessons.lesson_6.*;
+import ru.lessons.lesson_10v1.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,7 +20,10 @@ import java.util.concurrent.CopyOnWriteArrayList;
  **/
 
 public class ClinicServlet extends HttpServlet {
-    final List<Pet> pets = new CopyOnWriteArrayList<Pet>();
+    //final List<Pet> pets = new CopyOnWriteArrayList<Pet>();
+    Clinic clinic = new Clinic();
+    myArrayList<Client> clients = new myArrayList<Client>();
+    String searche;
     @Override
     protected void doGet (HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         resp.setContentType("text/html");
@@ -33,10 +36,16 @@ public class ClinicServlet extends HttpServlet {
                 "</head>" +
                 "<body>" +
                 "<form action='>" + req.getContextPath() + "/' method = 'post'>" +
-                "Name : <input type='text' name='name'>" +
-                "<input type='submit' value='Submit'>" +
-                "<form>" +
-                this.viewPets() +
+                "   Client Name : <input type='text' name='client'>" +
+                "   Dog Name : <input type='text' name='pet'>" +
+                "   <input type='submit' value='Submit'>" +
+                "</form>" +
+                this.viewClientsAndPets() +
+                "   <form action='>" + req.getContextPath() + "/' method = 'post'>" +
+                "   Client Searche : <input type='text' name='searche'>" +
+                "   <input type='submit' value='Submit'>" +
+                "</form>" +
+                this.searcheClient() +
                 "</body>" +
                 "</html>"
         );
@@ -46,10 +55,13 @@ public class ClinicServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
-        this.pets.add(new Dog(new Animal(req.getParameter("name"))));
+        //this.pets.add(new Dog(new Animal(req.getParameter("name"))));
+        clinic.addClient(new Client(req.getParameter("client"), new Dog(req.getParameter("pet"))));
+        searche = req.getParameter("searche");
         doGet(req, resp);
     }
 
+    /**
     private String viewPets() {
         StringBuilder sb = new StringBuilder();
         sb.append("<p>Pets</p>");
@@ -58,6 +70,42 @@ public class ClinicServlet extends HttpServlet {
             sb.append("<tr><td style='border : 1px solid black'>").append(pet.getName()).append("</td></tr>");
         }
         sb.append("</table>");
+        return sb.toString();
+    }
+    **/
+    private String searcheClient() {
+        StringBuilder sb = new StringBuilder();
+        if (searche != null) {
+            clients = clinic.findClientByName(searche);
+            sb.append("<p>Client Searche</p>");
+            sb.append("<table style='border : 1px solid black'>");
+            sb.append("<tr><td>N</td><td>Client</td><td>Pet</td><td>Vid</td></tr>");
+            for (int i = 0; i < clients.size(); i++) {
+                sb.append("<tr><td>" + i + "</td><td>" +
+                        clients.get(i).getClientId() + "</td><td>" +
+                        clients.get(i).getPetName() + "</td><td>" +
+                        clients.get(i).getPetType() + "</td></tr>");
+            }
+            sb.append("</table>");
+        }
+        return sb.toString();
+    }
+    private String viewClientsAndPets() {
+        StringBuilder sb = new StringBuilder();
+        if (clinic.getSize() > 0) {
+            clients = clinic.showClientAr();
+            sb.append("<p>Pets</p>");
+            sb.append("<table style='border : 1px solid black'>");
+            sb.append("<tr><td>N</td><td>Client</td><td>Pet</td><td>Vid</td></tr>");
+            for (int i = 0; i < clients.size(); i++) {
+                sb.append("<tr><td>" + i + "</td><td>" +
+                        clients.get(i).getClientId() + "</td><td>" +
+                        clients.get(i).getPetName() + "</td><td>" +
+                        clients.get(i).getPetType() + "</td></tr>");
+            }
+            //sb.append(clinic.showClientSt());
+            sb.append("</table>");
+        }
         return sb.toString();
     }
 }
